982c0392e799b8633ffe39a1d569c8007eb96431
skish1987 <seankish1@gmail.com>
2019-08-15 16:03:39 -0700

    models&jaws

--- models/example.js
+++ models/example.js
@@ -1,7 +1,41 @@
+// module.exports = function(sequelize, DataTypes) {
+//   var Recipes = sequelize.define("Recipes", {
+//     text: DataTypes.STRING,
+//     description: DataTypes.TEXT
+//   });
+//   var Users = sequelize.define("Users", {
+//     userName: DataTypes.STRING,
+//     password: DataTypes.STRING
+//   });
+//   return Recipes;
+// };
+
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
+  var Recipes = sequelize.define("Recipes", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    picture: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    typeOf: {
+      type: DataTypes.STRING,
+    },
+    ingredients: {
+      type: DataTypes.TEXT,
+      defaultValue: "None"
+    },
+    instructions: {
+      type: DataTypes.TEXT,
+    },
+    comments: {
+      type: DataTypes.TEXT,
+    },
   });
-  return Example;
+  return Recipes;
 };
