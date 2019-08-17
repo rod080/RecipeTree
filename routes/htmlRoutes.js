d747ed7257ef42775e97066cec611118168fde75
rod080 <49286063+rod080@users.noreply.github.com>
2019-08-17 11:45:51 -0700

    more stuff

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -22,6 +22,14 @@ module.exports = function(app) {
     });
   });
 
+  app.get("/recipe", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.render("recipeTemplate", {
+        msg: "Welcome!",
+        examples: dbExamples
+      });
+    });
+  });
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
